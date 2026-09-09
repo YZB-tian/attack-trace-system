cb10ea1fa649b80b2aacb3b28dea01fd3c9f83fa
honghaow607-stack <honghaow607@gmail.com>
2026-09-09 15:11:30 +0800

    Delete collectors/host_behavior/adapter.py

--- collectors/host_behavior/adapter.py
+++
@@ -1,6 +0,0 @@
-from typing import Iterable, Dict, Any, List
-from common.models import NormalizedEvent
-
-def normalize_host_behavior(records: Iterable[Dict[str, Any]], task_id: str) -> List[NormalizedEvent]:
-    """TODO: 返回 list[NormalizedEvent]。"""
-    return []
